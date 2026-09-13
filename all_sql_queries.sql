78f4ebb6369c383ae382ecd861d3fe7197577305
Petr Shulgat <petrshulgat@mailbox.unideb.hu>
2026-09-13 22:54:55 +0200

    write dag script

--- all_sql_queries.sql
+++ all_sql_queries.sql
@@ -11,18 +11,3 @@ rating text,
 duration text, 
 listed_in text, 
 description text);
-
-
-create table netflix_data_clean(
-show_id integer primary key, 
-type varchar(10), 
-title text, 
-director varchar(50), 
-"cast" text, 
-country varchar(50), 
-date_added date, 
-release_year integer, 
-rating text, 
-duration text, 
-listed_in text, 
-description text);
---
+++ dags/netflix_dag.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from datetime import datetime 
+
+from airflow import dag
+#Defines a task that runs Python
+from airflow.operators.python import PythonOperator 
+#Defines a task that runs SQL directly
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+#A connection helper, used inside Python code
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+def load_raw_data():
+
+    csv_path = "D:\study\Data_Engineering\netflix-data-warehouse\dags\netflix_dataset.csv"
+
+    df = pd.read_csv(csv_path)
+
+    # Get a connection to Postgres using the Airflow connection you created
+    hook = PostgresHook(postgres_conn_id="netflix_postgres")
+    engine = hook.get_sqlalchemy_engine()
+
+    df.to_sql(
+        "netflix_data_raw", engine, if_exists="replace", index=False
+    )
+
+with DAG(
+    dag_id = "netflix_dag",
+    description = "Load Netflix CSV into Postgres, then transform into a clean table",
+    start_date = datetime(2026, 9, 13),
+    schedule = None, # trigger manually
+    catchup = False
+) as dag:
+
+    load_raw = PythonOperator(
+        task_id = "load_raw_data"
+    )
+
+    transform = PostgresOperator(
+        task_id = "transform_raw_to_clean",
+        postgres_conn_id = "netflix_postgres",
+        sql = """
+            DROP TABLE IF EXISTS netflix_data_clean;
+
+            create table netflix_data_clean(
+                show_id integer primary key, 
+                type varchar(50), 
+                title text, 
+                director varchar(100), 
+                "cast" text, 
+                country varchar(50), 
+                date_added date, 
+                release_year integer, 
+                rating text, 
+                duration text, 
+                listed_in text, 
+                description text
+            )
+        """
+    )
